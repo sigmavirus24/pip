498c8cf31cc79f0c78467e5ed5cd75be13757c0f
Ian Cordasco <graffatcolmingov@gmail.com>
2014-01-10 22:02:29 -0600

    Add doc-string to test_get_file_content

--- tests/integration/test_download.py
+++ tests/integration/test_download.py
@@ -3,6 +3,12 @@ from pip.download import get_file_content, PipSession
 
 
 def test_get_file_content():
+    """
+    Test that given a URL, :func:`get_file_content` will download it.
+
+    More specifically, test that given an http URL and not a file URI that
+    this uses requests to successfully retrieve the text of the file.
+    """
     session = PipSession()
     with Betamax(session).use_cassette('get_file_content'):
         url, text = get_file_content('https://httpbin.org/get',
